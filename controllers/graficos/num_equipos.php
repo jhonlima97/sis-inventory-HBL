6fc12d3976cfa7dfb95604aa46d9ec2a28d0f55f
jhonlima97 <jwlimita08.997@gmail.com>
2024-10-06 23:27:11 -0500

    cambios

--- controllers/graficos/num_equipos.php
+++ controllers/graficos/num_equipos.php
@@ -5,7 +5,4 @@ try {
     $MG = new Model_Graficos(); // Instanciamos
     $consulta = $MG->getNumEquipos(); 
     echo json_encode($consulta); 
-} catch (Exception $e) {
-    echo json_encode(['error' => $e->getMessage()]);
-}
 ?>
